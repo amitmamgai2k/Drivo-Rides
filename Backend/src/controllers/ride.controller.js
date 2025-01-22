d2e8eebcd9c2c4df4d7917f498d41bf5614ec7cb
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-01-22 14:04:34 +0530

    f

--- Backend/src/controllers/ride.controller.js
+++ Backend/src/controllers/ride.controller.js
@@ -2,7 +2,7 @@ const rideService = require('../services/ride.service');
 const mapsService = require('../services/maps.service');
 const rideModel = require('../models/ride.model');
 const { validationResult } = require('express-validator');
-const { sendMessageToSocketId } = require('../socket');
+const { sendMessageToSocketId } = require('../../socket');
 
 const createRide = async (req, res) => {
     try {
