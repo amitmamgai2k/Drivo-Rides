1b5be79165141a421195ade33543589360356401
amitmamgai2k <123645217+amitmamgai2k@users.noreply.github.com>
2025-01-28 00:35:28 +0530

    Adding Features Like user Can share ride details and call

--- Frontend/src/components/WaitForDriver.jsx
+++ Frontend/src/components/WaitForDriver.jsx
@@ -3,6 +3,23 @@ import { Phone, Shield, Share } from 'lucide-react';
 
 const WaitForDriver = (props) => {
   console.log("WaitFor Driver",props.ride)
+  const shareRideHandler = () => {
+    if (navigator.share) {
+      const shareData = {
+
+        title: 'Share Ride Details',
+        text: `Ride Origin: ${props.ride.originText || 'unknown'}  Ride Destination: ${props.ride.destinationText || 'unknown'}  Captain Name: ${props.ride?.captain?.fullname?.firstname || 'unknown'} ${props.ride?.captain?.fullname?.lastname || 'unknown'}  Captain PhoneNumber: ${props.ride?.captain?.mobileNumber || 'unknown'}`,
+
+      };
+      console.log('Share Data:', shareData);
+      navigator.share(shareData)
+        .then(() => console.log('Ride shared successfully'))
+        .catch((error) => console.error('Error sharing ride:', error));
+    }
+    else {
+      console.log('Web Share API is not supported in this browser.');
+    }
+  }
 
   return (
     <div className="flex flex-col h-screen bg-white p-4">
@@ -31,7 +48,7 @@ const WaitForDriver = (props) => {
           <div className="absolute top-0 right-0 h-full w-4 bg-blue-600"></div>
         </div>
 
-        <p className="text-gray-900 font-bold">Maruti 800 {props.ride?.captain?.vehicle?.color} colour  {props.ride?.captain?.vehicle?.vehicleType}</p>
+        <p className="text-gray-900 font-bold">{props.ride?.captain?.vehicle?.model} {props.ride?.captain?.vehicle?.color} colour  {props.ride?.captain?.vehicle?.vehicleType}</p>
         <p className="text-2xl font-bold">{props.ride?.otp}</p>
         <p className='text-sm'>Share the Otp with the driver for ride confirmation</p>
       </div>
@@ -43,12 +60,14 @@ const WaitForDriver = (props) => {
             <p className="text-xs text-center">Safety</p>
           </div>
           <div>
-            <Share className="w-8 h-8 mx-auto mb-2 text-blue-500" />
+            <Share className="w-8 h-8 mx-auto mb-2 text-blue-500" onClick={shareRideHandler} />
             <p className="text-xs text-center">Share my trip</p>
           </div>
           <div>
             <Phone className="w-8 h-8 mx-auto mb-2 text-blue-500" />
-            <p className="text-xs text-center">Call driver</p>
+            <a href={`tel:${props.ride?.captain?.mobileNumber}`}>
+        <p className='text-center text-xs '>Call Now</p>
+    </a>
           </div>
         </div>
 
